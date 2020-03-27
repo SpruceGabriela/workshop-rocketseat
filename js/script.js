4cc49ac678fb93bd42c96fec903c8245a381c210
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2020-03-27 18:45:43 -0300

    creating ideas page

--- js/script.js
+++ js/script.js
@@ -3,4 +3,14 @@ function onOff(){
         .querySelector("#modal")
         .classList
         .toggle("hide")
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hide-scroll")
+
+    document 
+        .querySelector
+        .classList
+        .toggle("add-scroll")
 }
